b792241a37df8d2102edd647ccdbe365a1304432
jeff-k <jeff_k@fastmail.com>
2023-07-07 22:52:04 +0100

    start rewrite of translation module to support custom genetic codes

--- src/translation/mod.rs
+++ src/translation/mod.rs
@@ -2,41 +2,60 @@
 
 use crate::codec::{amino::Amino, dna::Dna, Codec};
 use crate::kmer::Kmer;
-use crate::seq::SeqSlice;
 
-impl From<Kmer<Dna, 3>> for Amino {
-    fn from(k: Kmer<Dna, 3>) -> Amino {
-        let x: usize = k.into();
-        Amino::unsafe_from_bits(x as u8)
+struct CustomCode {
+    table: Vec<Amino>,
+}
+
+impl CustomCode {
+    fn new(_table: Vec<(Kmer<Dna, 3>, Amino)>) -> Self {
+        unimplemented!()
     }
 }
 
-impl From<&Kmer<Dna, 3>> for Amino {
-    fn from(k: &Kmer<Dna, 3>) -> Amino {
-        let x: usize = k.into();
-        Amino::unsafe_from_bits(x as u8)
+impl TranslationTable for CustomCode {
+    fn translate(_codon: Kmer<Dna, 3>) -> Amino {
+        unimplemented!()
+    }
+
+    fn back_translate(_amino: Amino) -> Option<Kmer<Dna, 3>> {
+        unimplemented!()
     }
 }
 
-impl From<&SeqSlice<Dna>> for Amino {
-    fn from(seq: &SeqSlice<Dna>) -> Amino {
-        assert_eq!(seq.len(), 3);
-        Amino::unsafe_from_bits(seq.into())
+trait TranslationTable {
+    fn translate(codon: Kmer<Dna, 3>) -> Amino;
+    fn back_translate(amino: Amino) -> Option<Kmer<Dna, 3>>;
+}
+
+struct StandardCode;
+
+impl TranslationTable for StandardCode {
+    fn translate(codon: Kmer<Dna, 3>) -> Amino {
+        Amino::unsafe_from_bits(Into::<usize>::into(codon) as u8)
+    }
+
+    fn back_translate(_amino: Amino) -> Option<Kmer<Dna, 3>> {
+        None
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::prelude::*;
+    use crate::translation::StandardCode;
+    use crate::translation::TranslationTable;
 
     #[test]
     fn dna_to_amino() {
         let seq: Seq<Dna> = dna!("GCATGCGACGAATTCGGACACATAAAACTAATGAACCCACAAAGAAGCACAGTATGGTACTAA");
         let aminos: Seq<Amino> = seq
             .chunks(3)
-            .map(|codon| codon.into())
+            .map(|codon| StandardCode::translate(codon.into()))
             .collect::<Seq<Amino>>();
         assert_eq!(aminos, amino!("ACDEFGHIKLMNPQRSTVWY*"));
+        assert_ne!(aminos, amino!("ACDEFGHIKLMNPQRSTVWY*A"));
+        assert_ne!(aminos, amino!("CDEFGHIKLMNPQRSTVWY*A"));
     }
 
     #[test]
@@ -45,7 +64,7 @@ mod tests {
         let codons: Vec<Kmer<Dna, 3>> = seq.chunks(3).map(|codon| codon.into()).collect();
         let aminos: Seq<Amino> = codons
             .iter()
-            .map(|kmer| kmer.into())
+            .map(|kmer| StandardCode::translate(*kmer))
             .collect::<Seq<Amino>>();
         assert_eq!(aminos, amino!("SSSSSS***"));
     }
@@ -54,7 +73,10 @@ mod tests {
     fn test_debruin_sequence() {
         let seq: Seq<Dna> =
             dna!("AATTTGTGGGTTCGTCTGCGGCTCCGCCCTTAGTACTATGAGGACGATCAGCACCATAAGAACAAA");
-        let aminos: Seq<Amino> = seq.kmers().map(|kmer| kmer.into()).collect::<Seq<Amino>>();
+        let aminos: Seq<Amino> = seq
+            .kmers()
+            .map(|kmer| StandardCode::translate(kmer))
+            .collect::<Seq<Amino>>();
         assert_eq!(aminos.len(), 64);
         assert_eq!(
             aminos,
