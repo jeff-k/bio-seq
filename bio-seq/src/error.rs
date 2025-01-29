aed06f92eaa47c99d051f9879229a76c939bdab2
jeff-k <jeff_k@fastmail.com>
2025-01-29 00:48:54 +0000

    unit tests for error conditions

--- bio-seq/src/error.rs
+++ bio-seq/src/error.rs
@@ -44,3 +44,47 @@ impl From<ParseBioError> for std::io::Error {
 
 // #![feature(error_in_core)
 impl std::error::Error for ParseBioError {}
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+
+    #[test]
+    fn test_parse_error() {
+        let seq = Seq::<Dna>::from_str("ACGTx");
+        assert_eq!(seq, Err(ParseBioError::UnrecognisedBase(b'x')));
+
+        let seq = Seq::<Dna>::from_str("ACGT\n");
+        assert_eq!(seq, Err(ParseBioError::UnrecognisedBase(10)));
+
+        let err: std::io::Error = seq.unwrap_err().into();
+        assert_eq!(err.kind(), std::io::ErrorKind::Other);
+
+        let seq = Seq::<Dna>::from_str("ACGTT");
+        assert!(seq.is_ok());
+    }
+
+    #[test]
+    fn test_mismatched_length() {
+        let seq: &SeqSlice<Dna> = dna!("ACGTGAT");
+        let kmer = Kmer::<Dna, 14>::try_from(seq);
+        assert_eq!(kmer, Err(ParseBioError::MismatchedLength(14, 7)));
+
+        let kmer = Kmer::<Dna, 6>::try_from(seq);
+        assert_eq!(kmer, Err(ParseBioError::MismatchedLength(6, 7)));
+
+        let kmer = Kmer::<Dna, 7>::try_from(seq);
+        assert!(kmer.is_ok());
+    }
+
+    #[test]
+    fn test_size_error() {
+        let seq = dna!("ACGTACGTACGTACGTACGTACGTACGTACGTA");
+        let int = TryInto::<usize>::try_into(seq);
+
+        assert_eq!(int, Err(ParseBioError::SequenceTooLong(33, 32)));
+
+        let good = TryInto::<usize>::try_into(&seq[..32]);
+        assert!(good.is_ok());
+    }
+}
